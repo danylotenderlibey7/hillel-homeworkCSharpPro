030c27b0caf7b40a39eaee64000460213b4ee8b3
danylo <lysukdanil678@gmail.com>
2024-10-25 13:53:52 +0300

    Added 7.1 Homework

---
+++ 7.1/BarberShop.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _7._1
+{
+    public class BarberShop
+    {
+        Random random = new Random();
+        private static Semaphore waitingChair;// Семафор для ожидания -> кресло ожидания
+        private static Semaphore barberChair = new Semaphore(1, 1);// Семафор для кресла барбера
+        private static Semaphore barberSleepChair = new Semaphore(0, 1);// Семафор для контроля
+
+        private int _clientsServed = 0;
+        private int _totalClients;
+
+        public BarberShop(int waitingChairCount, int totalClients) 
+        { 
+            waitingChair = new Semaphore(waitingChairCount, waitingChairCount);
+            _totalClients = totalClients;
+        }
+
+        public void EnterBarberShop(int clientCount)//метод входа клиента в барбершоп
+        {
+            Console.WriteLine($"The client {clientCount} entered the barbershop");
+
+            if (waitingChair.WaitOne(0)) // Есть ли свободное место в креслах ожидания
+            {
+                Console.WriteLine($"The client {clientCount} is waiting on a waiting chair");
+                barberChair.WaitOne();// Клиент занимает кресло барбера
+                waitingChair.Release();// Освобождается место в креслах ожидания
+
+                Console.WriteLine($"The client {clientCount} is on a barber chair");
+                barberSleepChair.Release();// Барбер просыпаеться
+
+                Thread.Sleep(random.Next(1, 6) * 1000);//Время стрижки
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"The client {clientCount} left the barbershop");
+                Console.ResetColor();
+                barberChair.Release();//Кресло барбера освобождено 
+
+                Interlocked.Increment(ref _clientsServed);//Увеличивает счетчик обслуженых клиентов
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"The client {clientCount} didn't find a free chair and left");
+                Console.ResetColor();
+            }
+        }
+
+        public void StartBarber()//метод работы барбера
+        {
+            while(_clientsServed < _totalClients)
+            {
+                Console.WriteLine("The barber felt asleep");
+                barberSleepChair.WaitOne();//барбер ждет пока его не разбудят
+                Console.WriteLine("The barber woke up and started cutting");
+                Console.WriteLine();
+
+                Thread.Sleep(random.Next(1, 6) * 1000);//время стрижки
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("The barber has finished his work");
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+            Console.WriteLine("All clients have been served. The barber is going home.");
+        }
+    }
+}
---
+++ 7.1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalClients = new Random().Next(3, 10);
+            int waitingChairCount = new Random().Next(2, 5);
+            Console.WriteLine($"There are {totalClients} clients");
+            Console.WriteLine();
+
+            var barberShop = new BarberShop(waitingChairCount, totalClients); 
+            var barberThread = new Thread(barberShop.StartBarber);//поток для барбера
+            barberThread.Start();
+
+            List<Thread> clientThreads = new List<Thread>();
+
+            for (int i = 1; i <= totalClients; i++)//потоки для клиентов
+            {
+                int clientNumber = i;
+                var clientThread = new Thread(() => barberShop.EnterBarberShop(clientNumber));
+                clientThreads.Add(clientThread);//добавление потока в список
+                clientThread.Start();//запуск клиента
+
+                Thread.Sleep(new Random().Next(1, 3)*1000);//время прибытия другого клиента
+            }
+
+            foreach (var thread in clientThreads)//ожидания прибытия всех клиентов
+            {
+                thread.Join();
+            }
+
+            barberThread.Join();//ожидания конца работы барбера
+            Console.WriteLine("The barbershop is closed for the day.");
+        }
+    }
+}
